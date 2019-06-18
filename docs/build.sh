f743966eaab43a01638a7bca5a14abc1b6f3f5e1
Marlyn <mwilliams586@gmail.com>
2019-06-17 18:39:12 -0700

    adding docs folder and changes to website

---
+++ docs/build.sh
@@ -0,0 +1,6 @@
+cat ../template/top.html ../content/middle.html ../template/bottom.html > index.html
+cat ../template/contact_top.html ../content/contact_middle.html ../template/contact_bottom.html > contact.html
+cat ../template/resume_top.html ../content/resume_middle.html ../template/resume_bottom.html > resume.html
+cat ../template/bio_top.html ../content/bio_middle.html ../template/bio_bottom.html > bio.html
+
+
